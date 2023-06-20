95f2434f2f80ddc70086591806c7ec43a37b65f8
Ezel Espera <zellai022890@gmail.com>
2023-06-20 13:33:16 +0800

    Add user-id in the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
@@ -13,9 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         \App\Models\User::factory(5)->create();
+        //  \App\Models\User::factory(5)->create();
 
-         Listing::factory(6)->create();
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@gmail.com'
+        ]);
+
+         Listing::factory(6)->create([
+            'user_id' => $user->id
+         ]);
 
         //  Listing::create([
         //     'title' => 'Laravel Senior Developer', 
