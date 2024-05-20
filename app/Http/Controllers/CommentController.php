ee0f53b9a4a0f5e5c2d99e4cb0bbcee0c2de4763
Ezel Espera <zellai022890@gmail.com>
2024-05-20 14:17:43 +0800

    Redesign comment section

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -17,33 +17,33 @@ class CommentController extends Controller
 
     // Show Create Form
     // public function create($id){
-    //     $comment = Comment::find($id);  
+    //     $comment = Comment::find($id);
 
     //     return view('listings.create-comment')->with('comment', $comment);
     // }
 
     // Comment
     public function store(Request $request,$id, Comment $comment) {
-      
+
         $formFields['user_id'] = auth()->user()->id;
         $formFields['listing_id'] = $id;
         $formFields['userComment'] = $request->userComment;
-        // $formFields['name'] = auth()->user()->name; 
+        // $formFields['name'] = auth()->user()->name;
 
         if($request->hasFile('commentImage')) {
             $formFields['commentImage'] = $request->file('commentImage')->store('commentImages', 'public');
         }
 
 
-        Comment::create($formFields); 
+        Comment::create($formFields);
 
 
-        return redirect('/')->with('message', 'Comment created successfully!');
+        return redirect()->back()->with('message', 'Comment created successfully!');
     }
 
     public function edit(Comment $comment,$id, Listing $listing){
         $comment = $comment->find($id);
-        
+
         return view('listings.edit-comment', [
             'comment' => $comment,
             'listing' => $listing
@@ -74,17 +74,17 @@ class CommentController extends Controller
 
      // Delete Comment
      public function destroy($id, Listing $listing){
-    
+
 
         $comment = Comment::findOrFail($id);
-        
+
         $listing->user_id = auth()->user()->id;
         // dd(auth()->user()->id);
-    
+
         // Check if the authenticated user owns the comment and the listing
         if ((Auth::user()->id === $comment->user_id)||(Auth::user()->id === $listing->user_id)) {
             $comment->delete();
-    
+
             return back()->with('message', 'Comment deleted successfully.');
 
         } else {
--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -43,39 +43,15 @@
             <div class="border border-gray-200 w-full mb-6 mt-3"></div>
             <div>
                 <div class="text-lg space-y-6">
-
-                    {{-- {{$listing->id}} --}}
-
                     <form method="POST" action="/listings/{{$listing->id}}/comment" enctype="multipart/form-data">
-                    {{-- <form method="POST" action="{{ route('comments.store', $listing->id) }}" enctype="multipart/form-data"> --}}
                         @csrf
-                        <div class="bg-light p-2 pt-4">
-                            <!-- <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                                <a><i class="fa fa-message"></i> Comment </a>
-                            </td> -->
-                            {{-- <div>
-
-                                <label for="image" class="inline-block text-lg mb-4">
-                                    Author Image
-                                </label>
-                                <input
-                                    type="file"
-                                    class="border border-gray-200 rounded p-2"
-                                    name="image"
-                                />
-
-                                @error('image')
-                                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                                @enderror
-
-                            </div> --}}
-                             <div class="d-flex flex-row align-items-start">
-                                <img class="rounded-circle ms-3 me-6"
+                        <div class="bg-light p-2 pt-4 row">
+                            <div class="d-flex flex-row align-items-start col-lg-8">
+                                <img class="rounded-circle border border-dark ms-3 me-6"
                                     src="{{auth()->user()->userImage ? asset('storage/' . auth()->user()->userImage) : asset('Images/default-image.jpg')}}"
-                                    width="40"
+                                    width="50"
                                 >
-
-                                <input class="form-control shadow-none textarea me-6"
+                                <input class="form-control shadow-none textarea "
                                     name="userComment"
                                     placeholder="Add a comment..."
                                     rows="1">{{old('userComment')}}
@@ -86,80 +62,76 @@
                                 @enderror
                             </div>
 
-                            <div class="mt-2 text-right">
+                            <div class="mt-1 text-right col-lg-1">
                                 <button class="btn btn-primary btn-sm shadow-none me-6">
                                     Post comment
                                 </button>
                             </div>
                         </div>
                     </form>
-
-                        @unless(count($comments)==0)
-                        @foreach ($comments as $comment)
-                        @if($listing->id === $comment->listing_id)
-                        <div>
-                            <div class="container mt-5">
-                                <div class="d-flex justify-content-center row">
-                                    <div class="col-md-8">
-                                        <div class="d-flex flex-column comment-section">
-                                            <div class="bg-white p-2">
-
-                                                <div class="d-flex flex-row user-info">
-
-                                                    <img
-                                                        class="rounded-circle border border-dark"
-                                                        src="{{$comment->user->userImage ? asset('storage/' . $comment->user->userImage) : asset('Images/default-image.jpg')}}"
-                                                        width="40"
-                                                    >
-
-                                                    <div class="d-flex flex-column justify-content-start ml-2">
-                                                        <span class="d-flex d-block font-weight-bold name">{{$comment->user->name}}</span>
-                                                    </div>
+                    @unless(count($comments)==0)
+                    @foreach ($comments as $comment)
+                    @if($listing->id === $comment->listing_id)
+                    <div>
+                        <div class="container mt-5">
+                            <div class="d-flex justify-content-center row">
+                                <div class="col-md-8">
+                                    <div class="d-flex flex-column comment-section">
+                                        <div class="bg-white p-2">
+                                            <div class="d-flex flex-row user-info">
+                                                <img
+                                                    class="rounded-circle border border-dark"
+                                                    src="{{$comment->user->userImage ? asset('storage/' . $comment->user->userImage) : asset('Images/default-image.jpg')}}"
+                                                    width="40"
+                                                >
+                                                <div class="d-flex flex-column justify-content-start ml-2">
+                                                    <span class="d-flex d-block font-weight-bold name">{{$comment->user->name}}</span>
                                                 </div>
+                                            </div>
 
 
-                                                <div class="mt-2">
-                                                    <p class="d-flex comment-text">{{$comment->userComment}}</p>
-                                                </div>
+                                            <div class="mt-2">
+                                                <p class="d-flex comment-text">{{$comment->userComment}}</p>
+                                            </div>
 
-                                                <div class="bg-white">
-                                                    <div class="d-flex flex-row fs-12">
-                                                        @if(Auth::user()->id === $comment->user_id)
-                                                        <form>
-                                                        <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                                                            <a href="/edit-comment/{{$comment->id}}" class="text-blue-400 px-6 py-2 rounded-xl"><i
-                                                                class="fa-solid fa-pen-to-square"></i>
-                                                              Edit</a>
-                                                        </td>
-                                                        </form>
-                                                        <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                                                            <form method="POST" action="/comment/{{$comment->id}}">
-                                                                @csrf
-                                                                @method('DELETE')
-                                                                <button type="submit" class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>
-                                                            </form>
-                                                            </td>
-                                                        @elseif(Auth::user()->id === $listing->user_id)
-                                                        <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                                            <div class="bg-white">
+                                                <div class="d-flex flex-row fs-12">
+                                                    @if(Auth::user()->id === $comment->user_id)
+                                                    <form>
+                                                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                                                        <a href="/edit-comment/{{$comment->id}}" class="text-blue-400 px-6 py-2 rounded-xl"><i
+                                                            class="fa-solid fa-pen-to-square"></i>
+                                                            Edit</a>
+                                                    </td>
+                                                    </form>
+                                                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
                                                         <form method="POST" action="/comment/{{$comment->id}}">
                                                             @csrf
                                                             @method('DELETE')
                                                             <button type="submit" class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>
                                                         </form>
                                                         </td>
-                                                        @endif
-                                                    </div>
-
+                                                    @elseif(Auth::user()->id === $listing->user_id)
+                                                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                                                    <form method="POST" action="/comment/{{$comment->id}}">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>
+                                                    </form>
+                                                    </td>
+                                                    @endif
                                                 </div>
+
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        @endif
-                        @endforeach
-                        @endunless
+                    </div>
+                    @endif
+                    @endforeach
+                    @endunless
 
 
                     <a href="mailto:{{$listing->email}}" class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80">
