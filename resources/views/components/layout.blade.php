6b3b5063a275db1ac9c03af86fca2133cd6eed1e
Ezel Espera <zellai022890@gmail.com>
2024-05-21 09:34:26 +0800

    Import tailwind and Add dropdown settings

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
     <link rel="icon" href="images/favicon.ico" />
     <link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.min.css" />
     <link
@@ -19,7 +20,6 @@
         crossorigin="anonymous"
     />
     <script src="//unpkg.com/alpinejs" defer></script>
-    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
@@ -29,17 +29,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
-    <script>
-        tailwind.config = {
-            theme: {
-                extend: {
-                    colors: {
-                        laravel: "#ef3b2d",
-                    },
-                },
-            },
-        };
-    </script>
     <title>MyBlog | Ezel M. Espera</title>
 </head>
 <body class="mb-48">
@@ -90,18 +79,34 @@
                     </a>
                 </li>
                 <li class="nav-link" >
-                    <form class="nav-link" data-bs-toggle="tab" aria-selected="false" role="tab" tabindex="-1" method="POST" action="{{url('/logout')}}">
+                    <!-- <form class="nav-link" data-bs-toggle="tab" aria-selected="false" role="tab" tabindex="-1" method="POST" action="{{url('/logout')}}">
                     @csrf
                         <button type="submit">
                             <i class="fa-solid fa-door-closed"></i> Logout
                         </button>
-                    </form>
+                    </form> -->
                 </li>
                 <li class="nav-link" >
-                    <a class="nav-link" href="/users/{{auth()->user()->id}}/edit"  data-bs-toggle="tab" aria-selected="false" role="tab" tabindex="-1">
+                    <!-- <a class="nav-link" href="/users/{{auth()->user()->id}}/edit"  data-bs-toggle="tab" aria-selected="false" role="tab" tabindex="-1">
                         <i class="fa-solid fa-gear"></i>
                         Settings
-                    </a>
+                    </a> -->
+                    <div class="dropdown mr-1">
+                        <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-expanded="false" data-offset="10,20">
+                            <i class="fa-solid fa-gear"></i>
+                            Settings
+                        </button>
+                        <div class="dropdown-menu">
+                            <form class="dropdown-item" data-bs-toggle="tab" aria-selected="false" role="tab" tabindex="-1" method="POST" action="{{url('/logout')}}">
+                                @csrf
+                                <button type="submit">
+                                    <i class="fa-solid fa-door-closed"></i> Logout
+                                </button>
+                            </form>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
                 </li>
             </ul>
             @else
